8b684a089d23d0fe0e3f7da4254a198d42e66b8f
hujeong <hujeong@student.42seoul.kr>
2023-12-12 13:58:53 +0900

    Feat: Config 객체 파일  생성

---
+++ src/config.hpp
@@ -0,0 +1,18 @@
+#ifndef CONFIG_HPP
+#define CONFIG_HPP
+
+class Config
+{
+  private:
+    // 필요하면 만들죠
+    // int mServerNum; // 서버블록 개수 필요 없나? 없을수도 벡터 사이즈 들고 오면 되는 것 같긴한
+
+    // 굿굿 승효형 집중하세여
+  public:
+};
+
+#endif
+
+// 서버 블록 개수
+// 왜요?
+// vim 깔아줘
