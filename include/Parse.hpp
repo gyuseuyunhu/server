02f3a6afaf9bae106e35a5213ef5e9a54c4ea7b6
hujeong <hujeong@student.42seoul.kr>
2023-12-20 19:45:38 +0900

    feat: Parse 클래스 완성

--- include/Parse.hpp
+++ include/Parse.hpp
@@ -13,10 +13,17 @@ class Parse
     std::ifstream mFile;
     int mDepth;
     std::string mHttpStr;
-    std::vector<ServerLocPair> mServerLocPairs; // location 블록 경로는 LocationVec의 요소의 첫번째로 저장 ;으로 구분
+    std::vector<ServerLocPair> mServerLocPairs;
     void storeHttpStr(std::string &line);
     void storeServerStr(std::string &line);
     std::string storeLocationStr(std::string &line);
+
+    void skipBeforeHttp(std::string &line);
+    void skipDirective(std::string &line, int num);
+    void checkOpenBrace(std::string &line);
+    void checkCloseBrace(std::string &line);
+    void checkLocationPath(std::string &line, std::string &locationStr);
+    void parseElseLine(std::string &str, std::string &line);
 
   public:
     Parse(const char *path);
@@ -25,10 +32,3 @@ class Parse
 };
 
 #endif
-
-// 처음에 주석(#)이 오거나, 도중에 주석 왔을 때 처리
-// "{ }" ";"" 기준으로 잘라서 저장
-// 들어온 처음 키워드가 http, server, location 이 세개인지 검사
-//
-
-// 보내줄 형식 "지시어 내용 내용 내용"
