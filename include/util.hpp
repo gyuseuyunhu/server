945e07552e1d98e384696e56a92a2dd1fd28526f
hujeong <hujeong@student.42seoul.kr>
2023-12-30 15:41:45 +0900

    feat: util에 trim 추가

--- include/util.hpp
+++ include/util.hpp
@@ -5,6 +5,7 @@
 #include <string>
 
 bool isWhiteSpace(const char c);
+std::string trim(const std::string &str);
 void trimLine(std::string &line);
 void deleteComments(std::string &line);
 void ftGetLine(std::ifstream &file, std::string &line);
