ad20af1d22a1c5069de47f571e3612bafdc57745
hujeong <hujeong@student.42seoul.kr>
2023-12-30 15:36:25 +0900

    feat: event 종료 시  delete를 위한 handle 반환값 추가

--- include/AEvent.hpp
+++ include/AEvent.hpp
@@ -1,19 +1,23 @@
 #ifndef AEVENT_HPP
 #define AEVENT_HPP
 
+#include "Request.hpp"
 #include "Server.hpp"
+
+#define EVENT_FINISH 1
+#define EVENT_CONTINUE 0
 
 class AEvent
 {
   protected:
     // response
-    // request
+    Request mRequest;
     const Server &mServer;
 
   public:
     AEvent(const Server &server);
     virtual ~AEvent();
-    virtual void handle() = 0;
+    virtual int handle() = 0;
 };
 
 #endif
