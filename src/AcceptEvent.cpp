f9a2002d846dde19ad20601576f6f3a05d6d4da6
hujeong <hujeong@student.42seoul.kr>
2023-12-30 17:17:18 +0900

    fix: accept 에러 추가

--- src/AcceptEvent.cpp
+++ src/AcceptEvent.cpp
@@ -13,10 +13,14 @@ AcceptEvent::~AcceptEvent()
 
 int AcceptEvent::handle()
 {
-    // struct kevent newEvent;
-    // struct sockaddr_in clientAddr;
-    // socklen_t clientAddrSize = sizeof(clientAddr);
-    //  int clientSocket = accept(mServer.getSocket(), (struct sockaddr *)&clientAddr, &clientAddrSize);
+    struct kevent newEvent;
+    struct sockaddr_in clientAddr;
+    socklen_t clientAddrSize = sizeof(clientAddr);
+    int clientSocket = accept(mServer.getSocket(), (struct sockaddr *)&clientAddr, &clientAddrSize);
+    if (clientSocket == -1)
+    {
+        return EVENT_CONTINUE;
+    }
 
     fcntl(clientSocket, F_SETFL, O_NONBLOCK, FD_CLOEXEC);
     EV_SET(&newEvent, clientSocket, EVFILT_READ, EV_ADD, 0, 0, new ReadEvent(mServer, clientSocket));
