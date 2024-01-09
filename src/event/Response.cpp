b1a200a4982fc94e07d0a737861581db6cabb30b
hujeong <hujeong@student.42seoul.kr>
2024-01-09 22:52:59 +0900

    feat: addHead 오버로딩 및 init 메소드를 setStartLine으로 바꿈

--- src/event/Response.cpp
+++ src/event/Response.cpp
@@ -6,21 +6,23 @@ Response::Response() : mConnectionStatus(KEEP_ALIVE)
 {
 }
 
-void Response::init(int httpStatusCode, int contentLength)
+void Response::setStartLine(int httpStatusCode)
 {
     std::ostringstream oss;
     oss << "HTTP/1.1 " << httpStatusCode << " " << HttpStatusInfos::getHttpReason(httpStatusCode);
     mStartLine = oss.str();
+}
 
-    oss.str("");
-    oss.clear();
-    oss << "Content-Length: " << contentLength;
-    mHead = oss.str();
+void Response::addHead(const std::string &key, const int value)
+{
+    std::stringstream ss;
+    ss << value;
+    mHead += key + ": " + ss.str() + CRLF;
 }
 
 void Response::addHead(const std::string &key, const std::string &value)
 {
-    mHead += CRLF + key + ": " + value;
+    mHead += key + ": " + value + CRLF;
 }
 
 void Response::setBody(const std::string &body)
@@ -32,7 +34,7 @@ const std::string Response::toStr() const
 {
     assert(mStartLine.size() != 0);
     assert(mHead.size() != 0);
-    return mStartLine + CRLF + mHead + CRLF + CRLF + mBody;
+    return mStartLine + CRLF + mHead + CRLF + mBody;
 }
 
 const std::string &Response::getStartLine() const
