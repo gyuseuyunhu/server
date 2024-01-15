05f9d50c85586955542dcecc87ea4a464e22204b
hujeong <hujeong@student.42seoul.kr>
2024-01-15 23:12:13 +0900

    feat: 파이썬 콜론 수정

--- cgi-bin/upload.py
+++ cgi-bin/upload.py
@@ -10,11 +10,14 @@ def print_file_list():
     upload_dir = "./www/file"
     print("<ul>")
     for filename in os.listdir(upload_dir):
-        print(f"<div><a href=\"/file/{filename}\">{filename}</a></div>")
+        print(f'<div><a href="/file/{filename}">{filename}</a></div>')
     print("</ul>")
 
 # 추가된 부분: 파일 업로드 함수
 def handle_file_upload():
+    request_method = os.environ.get('REQUEST_METHOD', 'GET')
+    if request_method != 'POST':
+        return 
     form = cgi.FieldStorage()
     
     if 'file' in form:
@@ -44,22 +47,22 @@ try:
 
     # 폼과 파일 목록 출력
     print("Content-type: text/html\r\n\r\n")
-    print("<html lang=\"ko\">")
-    print("<head>")
-    print("<meta charset=\"UTF-8\"/>")
-    print("<title>파일 저장소</title>")
+    print('<html lang=\"ko\">')
+    print('<head>')
+    print('<meta charset="UTF-8"/>')
+    print('<title>파일 저장소</title>')
     print("</head>")
     print("<body>")
     print("<h2>파일 목록</h2>")
 
     # 업로드 폼 추가
-    print("""<form action="python/upload.py" method="post" enctype="multipart/form-data"">
+    print('''<form method="post" enctype="multipart/form-data">
           <label for="file">업로드할 파일을 선택하세요</label>
           <br>
           <input type="file" name="file" id="file" accept=".txt, .pdf, .docx, .gif, .jpeg">
           <br>
           <input type="submit" value="업로드">
-          </form>""")
+          </form>''')
 
     # 추가된 부분: 파일 업로드 함수 호출
     handle_file_upload()
