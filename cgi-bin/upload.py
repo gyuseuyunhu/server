979cb4cde88c373c952202ac31293c475cf1d750
seulee2 <croft@kakao.com>
2024-01-16 21:48:41 +0900

    feat: cgi upload, delete 기능 개선, 결과 상태코드 적용

--- cgi-bin/upload.py
+++ cgi-bin/upload.py
@@ -3,40 +3,40 @@
 
 import os
 import cgi
+
 # 파일 삭제 메소드
 def delete_file():
     file_info = os.environ.get('PATH_INFO', '')
     if file_info == '':
-        return
+        return 204, "No Content", "<h2>파일 삭제 실패</h2><p>존재하지 않는 파일입니다.</p>"
     upload_dir = os.environ.get('UPLOAD_DIR', '/')
     webserv_root = os.environ.get('WEBSERV_ROOT', '')
     file_path = webserv_root + upload_dir + file_info
     if os.path.exists(file_path):
         try:
             os.remove(file_path)
-            print("<h2>파일 삭제 성공</h2>")
-            print(f"<p>파일이 성공적으로 삭제되었습니다. 삭제된 파일: {file_info}</p>")
+            # 삭제 후 파일 목록 출력
+            return 202, "Accepted", "<h2>파일 삭제 성공</h2><p>파일을 삭제하는데 성공했습니다.</p>"
         except:
-            print("<h2>파일 삭제 실패</h2>")
-            print("<p>파일을 삭제하는데 실패하였습니다.</p>")
+            return 204, "No Content", "<h2>파일 삭제 실패</h2><p>파일을 삭제하는데 실패하였습니다.</p>"
     else:
-        print("<h2>파일 삭제 실패</h2>")
-        print("<p>존재하지 않는 파일입니다.</p>")
+        return 204, "No Content", "<h2>파일 삭제 실패</h2><p>존재하지 않는 파일입니다.</p>"
 
 # 파일 목록 출력
 def print_file_list():
     upload_dir = os.environ.get('WEBSERV_ROOT', '') + os.environ.get('UPLOAD_DIR', '/')
-    print("<ul>")
+    file_list_html = "<ul>"
     for filename in os.listdir(upload_dir):
-        print(f'<div><a href="/file/{filename}">{filename}</a>')
-        print(f'''
+        file_list_html += f'<div><a href="/file/{filename}">{filename}</a>'
+        file_list_html += f'''
             <form id="deleteForm" method="DELETE" action="python/upload.py/{filename}">
                 <input type="hidden" name="_method" value="DELETE">
                 <input type="submit" value="삭제">
             </form>
         </div>
-				''')
-    print("</ul>")
+        '''
+    file_list_html += "</ul>"
+    return file_list_html
 
 # 파일 업로드 함수
 def handle_file_upload():
@@ -50,67 +50,64 @@ def handle_file_upload():
             os.makedirs(upload_dir, exist_ok=True)
             file_path = os.path.join(upload_dir, file_item.filename)
 
-            if (os.path.exists(file_path)):
-                print("<h2>파일 업로드 실패</h2>")
-                print("<p>동일한 이름의 파일이 이미 존재합니다.</p>")
-                return
+            if os.path.exists(file_path):
+                return 400, "Bad Request", "<h2>파일 업로드 실패</h2><p>동일한 이름의 파일이 이미 존재합니다.</p>"
             with open(file_path, 'wb') as file:
                 file.write(file_item.file.read())
 
             # 파일 업로드 성공 메시지 출력
-            print("<h2>파일 업로드 성공</h2>")
-            print(f"<p>파일이 성공적으로 업로드되었습니다. 저장된 파일: <a href='/file/{file_item.filename}'>{file_item.filename}</a></p>")
+            upload_success_html = f"<h2>파일 업로드 성공</h2><p>파일이 성공적으로 업로드되었습니다. 저장된 파일: <a href='/file/{file_item.filename}'>{file_item.filename}</a></p>"
+            # 업로드 후 파일 목록 출력
+            return 201, "Created", upload_success_html
         else:
-            print("<h2>파일 업로드 실패</h2>")
-            print("<p>올바른 파일을 선택하세요.</p>")
+            return 400, "Bad Request", "<h2>파일 업로드 실패</h2><p>올바른 파일을 선택하세요.</p>"
     else:
-        print("<h2>파일 업로드 실패</h2>")
-        print("<p>파일이 선택되지 않았습니다.</p>")
+        return 400, "Bad Request", "<h2>파일 업로드 실패</h2><p>파일이 선택되지 않았습니다.</p>"
 
-# 폼과 파일 목록 출력
-status = 200
-code = "OK"
-print(f'Status: {status} {code}', end="\r\n")
-print('Content-type: text/html', end="\r\n\r\n")
-
-print('''
-<!DOCTYPE html>
-<html lang="ko">
-<head>
-<meta charset="UTF-8"/>
-<title>파일 저장소</title>
-</head>
-''')
-
-print('''
-<body>
-<h1>파일 저장소</h1>
-''')
-
-# 업로드 폼 추가
-print('''
-<form method="post" enctype="multipart/form-data">
-<label for="file">업로드할 파일을 선택하세요</label>
-<br>
-<input type="file" name="file" id="file" accept=".txt, .pdf, .docx, .gif, .jpeg">
-<input type="submit" value="업로드">
-</form>
-''')
+# HTML 페이지 출력
+def print_html_head():
+		print('''
+					<!DOCTYPE html>
+					<html lang="ko">
+							<head>
+								<meta charset="UTF-8"/>
+								<title>파일 저장소</title>
+							</head>
+							<body>
+								<h1>파일 저장소</h1>
+				''')
 
 # 파일 업로드 함수 호출
 if os.environ['REQUEST_METHOD'] == 'POST':
-    handle_file_upload()
-      
+    status, message, body = handle_file_upload()
+    print(f'Status: {status} {message}', end='\r\n')
+    print('Content-type: text/html', end='\r\n\r\n')
+    print_html_head()
+    print(body)
+    print('</body></html>')
+
 # 파일 삭제 함수 호출
 if os.environ['REQUEST_METHOD'] == 'DELETE':
-    delete_file()
+    status, message, body = delete_file()
+    print(f'Status: {status} {message}', end='\r\n')
+    print('Content-type: text/html', end='\r\n\r\n')
+
 
 # 파일 목록 출력
 if os.environ['REQUEST_METHOD'] == 'GET':
-		print_file_list()
-
-# 파일 삭제 메소드
-print('''
+    print(f'Status: 200 OK', end='\r\n')
+    print('Content-type: text/html', end='\r\n\r\n')
+    print_html_head()
+    print('''
+		<form method="post" enctype="multipart/form-data">
+    <label for="file">업로드할 파일을 선택하세요</label>
+    <br>
+    <input type="file" name="file" id="file" accept=".txt, .pdf, .docx, .gif, .jpeg">
+    <input type="submit" value="업로드">
+		</form>
+		''')
+    print(print_file_list())
+    print('''
 <script>
 document.addEventListener('DOMContentLoaded', function() {
     var deleteForms = document.querySelectorAll('#deleteForm');
@@ -121,9 +118,8 @@ document.addEventListener('DOMContentLoaded', function() {
             xhr.open('DELETE', form.action, true);
             xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
             xhr.onload = function() {
-                if (xhr.status === 200) {
-                    // 성공적으로 삭제되면 페이지를 다시 로드
-										alert("파일이 성공적으로 삭제되었습니다")
+                if (xhr.status === 202) {
+                    alert("파일이 성공적으로 삭제되었습니다");
                     location.reload();
                 }
             };
@@ -132,7 +128,6 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 });
 </script>
-''')
-    
-print('''</body>
-</html>''')
+''')			
+    print('</body></html>')
+
