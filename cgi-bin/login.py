dbd2718deff2ba09ef9ac3cee26de0a92976fba9
YunjooCho <yunjcho@student.42seoul.kr>
2024-01-17 21:53:46 +0900

    feat: 쿠키 세션 구현

---
+++ cgi-bin/login.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+import json
+import os
+import cgi
+
+# 딕셔너리 파일 경로
+dictionary_file_path = os.environ.get('WEBSERV_ROOT', '') + os.environ.get('UPLOAD_DIR', '/') + "member.json"
+
+def load_dictionary():
+    try:
+        with open(dictionary_file_path, 'r') as file:
+            return json.load(file)
+    except (IOError, ValueError):
+        return {}
+
+# 딕셔너리 파일 로드
+nickname_dictionary = load_dictionary()
+
+# 클라이언트로부터 전달된 쿠키 값 가져오기
+form = cgi.FieldStorage()
+user_cookie = form.getvalue('cookie')
+
+if not nickname_dictionary:
+    # 딕셔너리 파일이 없는 경우
+    print(f'Status: 200 OK', end='\r\n')
+    print("Content-type: text/plain", end='\r\n\r\n')
+    print("non-exist")
+elif user_cookie in nickname_dictionary:
+    # 딕셔너리에 쿠키 값이 있는 경우 해당 닉네임 반환
+    user_nickname = nickname_dictionary[user_cookie]
+    print(f'Status: 200 OK', end='\r\n')
+    print("Content-type: text/plain", end='\r\n\r\n')
+    print(user_nickname)
+else:
+    # 딕셔너리에 쿠키 값이 없는 경우
+    print(f'Status: 200 OK', end='\r\n')
+    print("Content-type: text/plain", end='\r\n\r\n')
+    print("non-exist")
